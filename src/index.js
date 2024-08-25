a0754fdaeb13bc4554e7153a9b1079f751e01496
Duongg-jp <itachi.hp9x@gmail.com>
2024-08-25 18:22:00 +0700

    install husky Prettier lint-staged

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-const { engine } = require("express-handlebars");
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
+const { engine } = require('express-handlebars');
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+const route = require('./routes');
 
-// http logger
-app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// handlebars
-app.engine("hbs", engine({
-  extname: ".hbs",
-  layoutsDir: path.join(__dirname, "resources", "views", "layouts"),
-  defaultLayout: "main"
-}));
-app.set("view engine", "hbs");
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.set("views", path.join(__dirname, "resources", "views"));
+// HTTP logger
+app.use(morgan('combined'));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// Handlebars setup
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+        layoutsDir: path.join(__dirname, 'resources', 'views', 'layouts'),
+        defaultLayout: 'main',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Route initialization
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
