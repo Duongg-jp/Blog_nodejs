7f8dc47a49ff13fb64a2c94041b7247ce6a812f7
Duongg-jp <itachi.hp9x@gmail.com>
2024-10-10 11:52:10 +0700

    sua cac loi nho lien quan den update dependencies

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,16 +1,11 @@
-import fetch from 'node-fetch'; // Sử dụng import cho node-fetch
-import Course from "../models/Course"; // Import mô hình Course
+const Course = require("../models/Course");
 
 class CourseController {
   // [GET] /courses/:slug
   show(req, res, next) {
     Course.findOne({ slug: req.params.slug })
       .then((course) => {
-        if (course) {
-          res.render("courses/show", { course });
-        } else {
-          res.status(404).send("Khóa học không tồn tại");
-        }
+        res.render("courses/show", { course });
       })
       .catch(next);
   }
@@ -25,6 +20,7 @@ class CourseController {
     Course.findById(req.params.id)
       .then((course) => {
         if (course) {
+          // Render view với dữ liệu course đã chuyển thành plain object
           res.render("courses/edit", { course: course.toObject() });
         } else {
           res.status(404).send("Khóa học không tồn tại");
@@ -34,47 +30,31 @@ class CourseController {
   }
 
   // [PORT] /courses/store
-  async store(req, res, next) {
+  store(req, res, next) {
     const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoID}/mqdefault.jpg`;
+    const course = new Course(req.body);
+    course
+      .save()
 
-    try {
-      const response = await fetch(`https://img.youtube.com/vi/${req.body.videoID}/maxresdefault.jpg`);
-      
-      if (response.ok) {
-        formData.image = `https://img.youtube.com/vi/${req.body.videoID}/maxresdefault.jpg`;
-      } else {
-        const fallbackResponse = await fetch(`https://img.youtube.com/vi/${req.body.videoID}/hqdefault.jpg`);
-        if (fallbackResponse.ok) {
-          formData.image = `https://img.youtube.com/vi/${req.body.videoID}/hqdefault.jpg`;
-        } else {
-          formData.image = 'https://example.com/default-image.jpg';
-        }
-      }
-
-      // Tạo một đối tượng Course mới và lưu vào cơ sở dữ liệu
-      const course = new Course(formData);
-      await course.save();
-
-      res.redirect("/");
-    } catch (error) {
-      console.error("Error fetching image:", error);
-      res.status(500).send("Error saving course. Please try again.");
-    }
+      .then(() => res.redirect("/"))
+      .catch((error) => {});
   }
 
-  // [PUT] /courses/:id
-  update(req, res, next) {
-    Course.updateOne({ _id: req.params.id }, req.body)
-      .then(() => res.redirect("/me/stored/courses"))
-      .catch(next);
-  }
-
-  // [DELETE] /courses/:id
-  delete(req, res, next) {
-    Course.deleteOne({ _id: req.params.id })
-      .then(() => res.redirect("back"))
-      .catch(next);
-  }
+// [PUT] /courses/:id
+update(req, res, next) {
+  Course.updateOne({ _id: req.params.id }, req.body) 
+  .then(() => res.redirect("/me/stored/courses"))
+  .catch(next);
 }
 
-export default new CourseController();
+// [delete] /courses/:id
+delete(req,res,next){
+  Course.deleteOne({ _id: req.params.id})
+  .then(() => res.redirect("back"))
+  .catch(next);
+}
+
+}
+
+module.exports = new CourseController();
--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-import express from "express";
-import { engine } from "express-handlebars";
-import path from "path";
-import methodOverride from "method-override";
-import morgan from "morgan";
-import db from "./config/db/index.js"; // Thêm .js vào cuối
-import route from "./routes/index.js"; // Thêm .js vào cuối
-
+const { engine } = require("express-handlebars");
+const path = require("path");
+const express = require("express");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
-// Kết nối đến DB
+const db = require("./config/db/index");
+
+// Connect to DB
 db.connect();
 
+const route = require("./routes");
+
 // Middleware
-app.use(express.static(path.join(process.cwd(), "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("combined"));
 
-// Thiết lập Handlebars với helpers
+// Handlebars setup with helpers
+// Đăng ký helper
 app.engine(
   "hbs",
   engine({
     extname: ".hbs",
-    layoutsDir: path.join(process.cwd(), "resources", "views", "layouts"),
+    layoutsDir: path.join(__dirname, "resources", "views", "layouts"),
     defaultLayout: "main",
     runtimeOptions: {
       allowProtoPropertiesByDefault: true,
@@ -40,12 +42,12 @@ app.engine(
   })
 );
 
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method'))
 
 app.set("view engine", "hbs");
-app.set("views", path.join(process.cwd(), "resources", "views"));
+app.set("views", path.join(__dirname, "resources", "views"));
 
-// Khởi tạo route
+// Route initialization
 route(app);
 
 app.listen(port, () => {
